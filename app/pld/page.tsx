1de526ba0b4d514b1f20fd39390e93838673a927
Luis Armando Alvarez Zapfe <lalvarezzapfe@gmail.com>
2026-03-16 23:00:28 -0600

    fix: useRef pld import

--- app/pld/page.tsx
+++ app/pld/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useState, useCallback } from 'react'
+import { useEffect, useState, useCallback, useRef } from 'react'
 import { createClient } from '@/lib/supabase-client'
 import React from 'react'
 
